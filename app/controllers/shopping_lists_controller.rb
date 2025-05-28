fc755e3f73fea5d6d2a3f9a66ec96280d6fc8676
Yaquelqun <sandjiv.parassou@gmail.com>
2025-05-28 14:57:08 +0200

    create new create_shopping_list service

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -14,11 +14,15 @@ class ShoppingListsController < ApplicationController
   end
 
   def create
-    @shopping_list = ShoppingList.create(name: params[:name])
-    @shopping_list.ingredients.insert_all(params[:missing_ingredients])
-    render json: {
+    service_result = CreateShoppingList.new("Shopping List for #{params[:name]}", params[:ingredients]).call
+    case service_result
+    in { status: :error, message: message }
+      render json: { error: message }, status: :unprocessable_entity
+    else
+      render json: {
       message: "Shopping List created successfully",
-      shopping_list: @shopping_list
+      shopping_list: service_result
     }
+    end
   end
 end
--- app/services/cook_recipe.rb
+++ app/services/cook_recipe.rb
@@ -15,15 +15,7 @@ class CookRecipe
       fridge.remove_ingredients(recipe.ingredients)
       { enough_ingredients: true }
     else
-      { enough_ingredients: false, shopping_list_id: create_shopping_list_with(missing_ingredients).id }
+      { enough_ingredients: false, shopping_list_id: CreateShoppingList.new("Shopping List for #{recipe.name}", missing_ingredients).call.id }
     end
-  end
-
-  private
-
-  def create_shopping_list_with(missing_ingredients)
-    list = ShoppingList.create(name: "Shopping List for #{recipe.name}", done: false)
-    list.ingredients.insert_all(missing_ingredients)
-    list
   end
 end

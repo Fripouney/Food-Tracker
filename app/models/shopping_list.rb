52df07266d45d7c1463b07ee40841aa7fee52c51
Yaquelqun <sandjiv.parassou@gmail.com>
2025-05-28 14:45:33 +0200

    finish cook_recipe service

--- app/models/shopping_list.rb
+++ app/models/shopping_list.rb
@@ -1,6 +1,6 @@
 class ShoppingList < ApplicationRecord
   validates :name, presence: true
-  validates :done, presence: true
+  validates :done, inclusion: { in: [true, false] }
   has_many :ingredients, as: :ingredientable
 
   def add_ingredient(ingredient)
--- app/services/cook_recipe.rb
+++ app/services/cook_recipe.rb
@@ -15,7 +15,15 @@ class CookRecipe
       fridge.remove_ingredients(recipe.ingredients)
       { enough_ingredients: true }
     else
-      { enough_ingredients: false, shopping_list_id: create_shopping_list(missing_ingredients) }
+      { enough_ingredients: false, shopping_list_id: create_shopping_list_with(missing_ingredients).id }
     end
+  end
+
+  private
+
+  def create_shopping_list_with(missing_ingredients)
+    list = ShoppingList.create(name: "Shopping List for #{recipe.name}", done: false)
+    list.ingredients.insert_all(missing_ingredients)
+    list
   end
 end
